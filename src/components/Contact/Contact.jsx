dbf0e51f76af4db32f648ca54cc1b22002d2b733
AlexxSich <alexxsich@gmail.com>
2024-06-09 20:04:18 -0300

    updated ContactList

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,42 +1,33 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contactsOps';
-import { selectFilteredContacts } from '../../redux/contactsSlice';
 import { FaUser, FaPhoneAlt } from 'react-icons/fa';
 import css from './Contact.module.css';
 
-export default function Contact() {
+export default function Contact({ contact }) {
   const dispatch = useDispatch();
 
   const handleDelete = contactId => {
     dispatch(deleteContact(contactId));
   };
 
-  const visibleContacts = useSelector(selectFilteredContacts);
-
   return (
-    <ul className={css.contactsList}>
-      {visibleContacts.map(contact => (
-        <li className={css.contactsItem} key={contact.id}>
-          <div className={css.contactContainer}>
-            <div className={css.contacts}>
-              <p className={css.contactsItem}>
-                <FaUser className={css.myIcon} />
-                {contact.name}
-              </p>
-              <p className={css.contactsItem}>
-                <FaPhoneAlt className={css.myIcon} />
-                {contact.number}
-              </p>
-            </div>
-            <button
-              className={css.contactBtn}
-              onClick={() => handleDelete(contact.id)}
-            >
-              Delete
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className={css.contactContainer}>
+      <div className={css.contacts}>
+        <p className={css.contactsItem}>
+          <FaUser className={css.myIcon} />
+          <span className={css.contactName}>{contact.name}</span>
+        </p>
+        <p className={css.contactsItem}>
+          <FaPhoneAlt className={css.myIcon} />
+          {contact.number}
+        </p>
+      </div>
+      <button
+        className={css.contactBtn}
+        onClick={() => handleDelete(contact.id)}
+      >
+        Delete
+      </button>
+    </div>
   );
 }
